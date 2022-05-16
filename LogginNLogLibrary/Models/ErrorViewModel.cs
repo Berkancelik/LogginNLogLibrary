00dc2125d9c30e5e291475a4aa8cb0700f8c8fb8
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-16 08:08:34 +0300

    Add project files.

---
+++ LogginNLogLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LogginNLogLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
